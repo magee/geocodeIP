f2315f8d5a16e353b608b9a248fba2f27e7f2c32
Magee Mooney <mageemooney@gmail.com>
2013-09-19 16:04:01 -0700

    another try at solving heroku jade templating problem

--- app.js
+++ app.js
@@ -12,18 +12,17 @@ var connectTimeout = require('connect-timeout');
 
 var app = express();
 
-app.set('port', process.env.PORT || 3003);
+app.set('port', process.env.PORT || 3000);
 app.use(connectTimeout({ time: 3000 }));
 app.use(app.router);
 
 //app.use(express['static'](path.join(__dirname, '/public')));
 app.use(express.static(__dirname + '/public'));
 console.log('dirname: ', __dirname)
+
 app.set('views', __dirname + '/views');
 console.log('dirname: ', __dirname + '/views');
 app.set('view engine', 'jade'); //extension of views
-
-
 
 app.get('/', routes.index);
 app.get('/location/:ip', geocodes.findByIP);
